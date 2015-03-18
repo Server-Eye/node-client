e35ef8d2c60a52e0c494a312d0b1e66d67cb1fc3
SockeThe2nd <roland.paltz@gmx.net>
2015-03-18 15:13:26 +0100

    adds container basefunctions

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ var API = require("node-se-API-connector");
 var loginData = require('./logindata.json');
 
 //For debug
-console.log(API);
-console.log(loginData);
+console.log("API-Object: ", API);
+console.log("LoginData: ", loginData);
 
 API.setAPIkey(loginData.apiKey);
 
-API.container.get(loginData.containerID).then(function(res){
+/*
+API.container.create({
+    name: "TestName",
+    realName: "TestRealName",
+    type: 4,
+    parentId: loginData.containerID
+}).then(function(res){
+    console.log(res);
+}).fail(function(res){
     console.log(res);
 });
+*/
