263dac5f4b26fb7e20080b9cce7ae21f18050609
SockeThe2nd <roland.paltz@gmx.net>
2015-03-24 14:31:30 +0100

    Adds pushloop

--- index.js
+++ index.js
@@ -9,6 +9,39 @@ var now = new Date();
 
 API.setAPIkey(loginData.apiKey);
 
+API.container.get(loginData.container_id).then(function(container){
+    console.log(container);
+})
+
+API.push.start(loginData.container_id).then(function(emitter){
+    console.log("Got emitter");
+    emitter.on('connect', function(){
+        console.log("Emitter: Got connect");
+    });
+    emitter.on('data',function(data){
+        console.log("Emitter: got data");
+        API.push.result(data.containerId, data.messageId, true).then(function(){
+            console.log("Pushed result!");
+        });
+    });
+    emitter.on('error', function(reason){
+        console.log("Emitter: got error ", reason);
+    });
+    emitter.on('disconnect', function(reason){
+        console.log("Emitter: got disconnect ", reason);
+    });
+}).fail(function(reason){
+    console.log(reason);
+});
+
+setTimeout(function(){
+    console.log("Timeout triggered");
+    API.push.stop(loginData.container_id);
+},600000);
+
+
+
+/*
 API.agent.notify(loginData.agent_id, loginData.container_id, {
     guid: loginData.agent_id,
     error: true,
@@ -20,8 +53,6 @@ console.log(res);
 API.agent.get(loginData.agent_id).then(function(res){console.log(res);}).fail(function(res){console.log(res);});}).fail(function(res){console.log(res);});
 
 //API.agent.get(loginData.agent_id).then(function(res){console.log(res);}).fail(function(res){console.log(res);});
-
-/*
         new API.classes.Agent(loginData.container_id, "EXTERNAL-SOFTWARE", "TESTAGENT");
 
 API.agent.post(testAgent).then(function(res){console.log(res);}).fail(function(res){console.log(res);});
